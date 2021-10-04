8a4cc251db094b4876d3200f4234a0611ed688c1
Evan Engineers <evanengineers@gmail.com>
2021-10-04 12:38:53 -0500

    revision 3.0. created the web module along with the webMain class, with the viewnames class and webConfig class. Created the "home" template into to setup the home page for the game on the web module. Added thymeleaf config to the thymeleaf home view and added the h1 and href.

---
+++ web/src/main/java/evan/learnprogramming/config/WebConfig.java
@@ -0,0 +1,15 @@
+package evan.learnprogramming.config;
+
+import evan.learnprogramming.util.ViewNames;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+}
---
+++ web/src/main/java/evan/learnprogramming/util/ViewNames.java
@@ -0,0 +1,10 @@
+package evan.learnprogramming.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames() {}
+}
