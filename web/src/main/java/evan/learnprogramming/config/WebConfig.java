8f9b83ef38c2d243f5219493cf00b2ea6b73c44d
Evan Engineers <evanengineers@gmail.com>
2021-10-27 18:52:09 -0500

    revision 3.0. Added the locale change interceptor in WebConfig so that the the app would use the session to get the locale param language.  This is so we won't have to manually comment or uncomment in the application.properties file.

--- web/src/main/java/evan/learnprogramming/config/WebConfig.java
+++ web/src/main/java/evan/learnprogramming/config/WebConfig.java
@@ -2,13 +2,23 @@ package evan.learnprogramming.config;
 
 import evan.learnprogramming.interceptor.RequestInterceptor;
 import evan.learnprogramming.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    // == bean methods ==
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -18,5 +28,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestInterceptor());
+
+//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+//        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(new LocaleChangeInterceptor());
     }
+
 }
