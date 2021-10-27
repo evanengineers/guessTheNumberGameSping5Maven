66e2bb4a30fa9f54e3586959acbda69bc3052c1b
Evan Engineers <evanengineers@gmail.com>
2021-10-26 21:03:29 -0500

    revision 3.0. implemented handler request interception with RequestInterceptor class and WebConfig

--- web/src/main/java/evan/learnprogramming/config/WebConfig.java
+++ web/src/main/java/evan/learnprogramming/config/WebConfig.java
@@ -1,7 +1,9 @@
 package evan.learnprogramming.config;
 
+import evan.learnprogramming.interceptor.RequestInterceptor;
 import evan.learnprogramming.util.ViewNames;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
