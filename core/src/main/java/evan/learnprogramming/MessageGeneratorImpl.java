ad137391c24f8dbbe49b83d55891fe78b67b25ce
Evan Engineers <evanengineers@gmail.com>
2021-10-26 20:37:32 -0500

    revision 3.0. completed the message gnerator internationalization for result messages in the MessageGeneratorImpl, messages_es.properties and the application.properties files.

--- core/src/main/java/evan/learnprogramming/MessageGeneratorImpl.java
+++ core/src/main/java/evan/learnprogramming/MessageGeneratorImpl.java
@@ -1,6 +1,8 @@
 package evan.learnprogramming;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,12 +10,25 @@ import javax.annotation.PostConstruct;
 @Slf4j
 @Component
 public class MessageGeneratorImpl implements MessageGenerator {
+
+    // == contants ==
+    private static final String MAIN_MESSAGE = "game.main.message";
+    private static final String WIN = "game.win";
+    private static final String  LOSE = "game.lose";
+    private static final String INVALID_RANGE = "game.invalid.range";
+    private static final String FIRST_GUESS = "game.first.guess";
+    private static final String HIGHER = "game.higher";
+    private static final String LOWER = "game.lower";
+    private static final String REMAINING = "game.remaining";
+
     // == fields ==
     private final Game game;
+    private final MessageSource messageSource;
 
     // == Constructors ==
-    public MessageGeneratorImpl(Game game) {
+    public MessageGeneratorImpl(Game game, MessageSource messageSource) {
         this.game = game;
+        this.messageSource = messageSource;
     }
 
     // == init ==
@@ -25,30 +40,42 @@ public class MessageGeneratorImpl implements MessageGenerator {
     // == public methods ==
     @Override
     public String getMainMessage() {
-        return "The number is between " +
-                game.getSmallest() +
-                " and " +
-                game.getBiggest() +
-                ". Can you guess it?";
+        return getMessage(MAIN_MESSAGE, game.getSmallest(), game.getBiggest());
+        //The code below was used before the we implemented internationalization with the game.main.message key.
+//        return "The number is between " +
+//                game.getSmallest() +
+//                " and " +
+//                game.getBiggest() +
+//                ". Can you guess it?";
     }
 
     @Override
     public String getResultMessage() {
         if (game.isGameWon()) {
-            return "You guessed it! The number was " + game.getNumber();
+            return getMessage(WIN, game.getNumber());
         } else if (game.isGameLost()) {
-            return "You LOST!. The number was " + game.getNumber();
+            return getMessage(LOSE, game.getNumber());
         } else if (!game.isValidNumberRange()) {
-            return "Invalid number range!";
+            return getMessage(INVALID_RANGE);
         } else if (game.getRemainingGuesses() == game.getGuessCount()) {
-            return "What is your first guess?";
+            return getMessage(FIRST_GUESS);
         } else {
-            String direction = "Lower";
+            String direction = getMessage(LOWER);
 
             if (game.getGuess() < game.getNumber()) {
-                direction = "Higher";
+                direction = getMessage(HIGHER);
             }
-            return direction + "! You have " + game.getRemainingGuesses() + " guesses left.";
+            return getMessage(REMAINING, direction, game.getRemainingGuesses());
         }
     }
+
+    // == private methods ==
+    private String getMessage (String code, Object... args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+        //calling method getMessage from the messageSource Instance with a parameter code
+        // The code param reps the property key like game.main.message
+        //The second param is our method (Object... args.  The 3 periods after the object means that were using a variable number of arguments)
+        //The third arg is the locale which needs to be specified so that the message source can get us the message used in the specific locale
+    }
+
 }
